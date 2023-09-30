0826d1d6086a0fcd9b96303945706770dce18bcf
Warnd <dzerdevwarnd@gmail.com>
2023-09-30 18:03:01 +0300

    DeployFix06

--- src/routers/testingRouter.ts
+++ src/routers/testingRouter.ts
@@ -1,22 +1,17 @@
 import { Request, Response, Router } from 'express'
-import { basicAuthMiddleware } from '../middleware/authMiddleware'
 import { posts } from '../repositories/PostsRepository'
 import { blogs } from '../repositories/blogsRepository'
 
 export const testingRouter = Router({})
 
-testingRouter.delete(
-	'/all-data',
-	basicAuthMiddleware,
-	(req: Request, res: Response): void => {
-		if ((!posts || posts.length === 0) && (!blogs || blogs.length === 0)) {
-			res.sendStatus(404)
-			return
-		} else {
-			posts.splice(0, posts.length)
-			blogs.splice(0, blogs.length)
-			res.sendStatus(204)
-			return
-		}
+testingRouter.delete('/all-data', (req: Request, res: Response): void => {
+	if ((!posts || posts.length === 0) && (!blogs || blogs.length === 0)) {
+		res.sendStatus(404)
+		return
+	} else {
+		posts.splice(0, posts.length)
+		blogs.splice(0, blogs.length)
+		res.sendStatus(204)
+		return
 	}
-)
+})
