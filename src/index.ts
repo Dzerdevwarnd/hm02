14b7a7d46d939624b373926f94321bd2df94b942
Warnd <dzerdevwarnd@gmail.com>
2023-09-30 16:38:18 +0300

    hm02Deploy

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,6 @@
-import express from 'express'
-import { blogsRouter } from './routers/blogsRouter'
-import { postsRouter } from './routers/postsRouter'
-import { testingRouter } from './routers/testingRouter'
+import { app } from './setting'
 
-export const app = express()
 const port = process.env.PORT || 3004
-
-app.use(express.json())
-
-app.use('/blogs', blogsRouter)
-app.use('/posts', postsRouter)
-app.use('/testing', testingRouter)
 
 app.listen(port, () => {
 	console.log(`Example app listening on port ${port}`)
